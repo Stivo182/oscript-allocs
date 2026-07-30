90e6f874fd34d6b289ed1e4a0803762f5205316b
Dmitry Ivanov <ivanovdmitry91@gmail.com>
2026-07-30 17:38:34 +0300

    fix: стабилизировать измерение аллокаций и добавить диагностический стенд
    Переработать получение количества выделенных байт для поддерживаемых
    версий .NET и исключить влияние принудительной сборки мусора на результаты
    измерений.
    
    Изменения в МонитореПамяти:
    
    - использовать GC.GetTotalAllocatedBytes(precise: true) под .NET 8;
    - использовать MonitoringTotalAllocatedMemorySize под .NET Framework 4.8;
    - переключаться на GC.GetAllocatedBytesForCurrentThread, если мониторинг
      AppDomain недоступен, например при работе под Mono;
    - убрать рефлексию и вспомогательные делегаты для поиска методов GC;
    - убрать принудительный вызов GC.Collect перед чтением счётчика;
    - хранить начальное значение счётчика в long;
    - добавить явное состояние активного измерения;
    - запрещать повторный вызов Начать до завершения текущего замера;
    - запрещать вызов Завершить без предварительного Начать;
    - запрещать повторное завершение одного замера;
    - выдавать диагностическую ошибку при уменьшении счётчика, которое может
      указывать на выполнение Начать и Завершить в разных потоках;
    - перестать маскировать некорректную отрицательную дельту через Math.Max.

--- src/MemoryMonitor.cs
+++ src/MemoryMonitor.cs
@@ -1,12 +1,13 @@
-﻿// Portions of this code are derived from `BenchmarkDotNet` (https://github.com/dotnet/BenchmarkDotNet),
-// Copyright (c) 2013–2024 .NET Foundation and contributors. MIT License.
-
-using System;
-using System.Reflection;
+﻿using System;
 using ScriptEngine.Machine.Contexts;
 
-#if NET6_0_OR_GREATER
+#if NET8_0_OR_GREATER
+using OneScript.Commons;
 using OneScript.Contexts;
+using OneScript.Exceptions;
+#elif NET48
+using ScriptEngine;
+using ScriptEngine.Machine;
 #endif
 
 namespace AllocsOneScript
@@ -14,7 +15,8 @@ namespace AllocsOneScript
     [ContextClass("МониторПамяти", "MemoryMonitor")]
     public class MemoryMonitor : AutoContext<MemoryMonitor>
     {
-        private decimal _StartAllocatedBytes;
+        private long _startAllocatedBytes;
+        private bool _isStarted;
 
         [ScriptConstructor]
         public static MemoryMonitor Constructor()
@@ -25,119 +27,76 @@ namespace AllocsOneScript
         [ContextMethod("Начать", "Start")]
         public void Start()
         {
-            _StartAllocatedBytes = (decimal)GetAllocatedBytes();
+            if (_isStarted)
+                throw new RuntimeException(
+                    Locale.NStr(
+                        "ru = 'Замер памяти уже начат'; en = 'Memory measurement has already been started'"));
+
+            _startAllocatedBytes = GetAllocatedBytes();
+            _isStarted = true;
         }
 
         [ContextMethod("Завершить", "Stop")]
         public decimal Stop()
         {
-            return Math.Max(0, (decimal)GetAllocatedBytes() - _StartAllocatedBytes);
+            if (!_isStarted)
+                throw new RuntimeException(
+                    Locale.NStr(
+                        "ru = 'Замер памяти не был начат'; en = 'Memory measurement has not been started'"));
+
+            long endAllocatedBytes = GetAllocatedBytes();
+            _isStarted = false;
+
+            if (endAllocatedBytes < _startAllocatedBytes)
+                throw new RuntimeException(
+                    Locale.NStr(
+                        "ru = 'Счётчик аллокаций уменьшился; Start и Stop могли выполниться на разных потоках'; " +
+                        "en = 'The allocation counter decreased; Start and Stop may have run on different threads'"));
+
+            return endAllocatedBytes - _startAllocatedBytes;
         }
 
         [ContextMethod("РазмерКучи", "HeapSize")]
         public decimal HeapSize() => GC.GetTotalMemory(true);
 
         [ContextMethod("ВсегоВыделеноБайт", "TotalAllocatedBytes")]
-        public decimal GetTotalAllocatedBytes() => (decimal)GetAllocatedBytes();
+        public decimal GetTotalAllocatedBytes() => GetAllocatedBytes();
 
-        private long? GetAllocatedBytes()
+        private long GetAllocatedBytes()
         {
-            // "This instance Int64 property returns the number of bytes that have been allocated by a specific
-            // AppDomain. The number is accurate as of the last garbage collection." - CLR via C#
-            // so we enforce GC.Collect here just to make sure we get accurate results
-            GC.Collect();
-
-#if NET6_0_OR_GREATER
+#if NET8_0_OR_GREATER
             return GC.GetTotalAllocatedBytes(precise: true);
-#else
-            if (GcHelpers.GetTotalAllocatedBytesDelegate != null) // it's .NET Core 3.0 with the new API available
-                return GcHelpers.GetTotalAllocatedBytesDelegate.Invoke(true); // true for the "precise" argument
-
-            if (GcHelpers.CanUseMonitoringTotalAllocatedMemorySize) // Monitoring is not available in Mono, see http://stackoverflow.com/questions/40234948/how-to-get-the-number-of-allocated-bytes-
+#elif NET48
+            if (CanUseMonitoringTotalAllocatedMemorySize)
                 return AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize;
 
-            if (GcHelpers.GetAllocatedBytesForCurrentThreadDelegate != null)
-                return GcHelpers.GetAllocatedBytesForCurrentThreadDelegate.Invoke();
-
-            return null;
+            // AppDomain monitoring is unavailable on Mono. The net48 reference
+            // assemblies expose this API directly, so reflection is unnecessary.
+            return GC.GetAllocatedBytesForCurrentThread();
+#else
+#error Unsupported target framework
 #endif
         }
 
-#if !NET6_0_OR_GREATER
-        // Separate class to have the cctor run lazily, to avoid enabling monitoring before the benchmarks are ran.
-        private static class GcHelpers
+#if NET48
+        private static readonly bool CanUseMonitoringTotalAllocatedMemorySize =
+            TryEnableMonitoringTotalAllocatedMemorySize();
+
+        private static bool TryEnableMonitoringTotalAllocatedMemorySize()
         {
-            // do not reorder these, CheckMonitoringTotalAllocatedMemorySize relies on GetTotalAllocatedBytesDelegate being initialized first
-            public static readonly Func<bool, long> GetTotalAllocatedBytesDelegate = CreateGetTotalAllocatedBytesDelegate();
-            public static readonly Func<long> GetAllocatedBytesForCurrentThreadDelegate = CreateGetAllocatedBytesForCurrentThreadDelegate();
-            public static readonly bool CanUseMonitoringTotalAllocatedMemorySize = CheckMonitoringTotalAllocatedMemorySize();
-
-            private static Func<bool, long> CreateGetTotalAllocatedBytesDelegate()
+            try
             {
-                try
-                {
-                    // this method is not a part of .NET Standard so we need to use reflection
-                    var method = typeof(GC).GetTypeInfo().GetMethod("GetTotalAllocatedBytes", BindingFlags.Public | BindingFlags.Static);
+                if (!AppDomain.MonitoringIsEnabled)
+                    AppDomain.MonitoringIsEnabled = true;
 
-                    if (method == null)
-                        return null;
-
-                    // we create delegate to avoid boxing, IMPORTANT!
-                    var del = (Func<bool, long>)method.CreateDelegate(typeof(Func<bool, long>));
-
-                    // verify the api works
-                    return del.Invoke(true) >= 0 ? del : null;
-                }
-                catch
-                {
-                    return null;
-                }
+                return AppDomain.MonitoringIsEnabled
+                    && AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize >= 0;
             }
-
-            private static Func<long> CreateGetAllocatedBytesForCurrentThreadDelegate()
+            catch
             {
-                try
-                {
-                    // this method is not a part of .NET Standard so we need to use reflection
-                    var method = typeof(GC).GetTypeInfo().GetMethod("GetAllocatedBytesForCurrentThread", BindingFlags.Public | BindingFlags.Static);
-
-                    if (method == null)
-                        return null;
-
-                    // we create delegate to avoid boxing, IMPORTANT!
-                    var del = (Func<long>)method.CreateDelegate(typeof(Func<long>));
-
-                    // verify the api works
-                    return del.Invoke() >= 0 ? del : null;
-                }
-                catch
-                {
-                    return null;
-                }
-            }
-
-            private static bool CheckMonitoringTotalAllocatedMemorySize()
-            {
-                try
-                {
-                    // we potentially don't want to enable monitoring if we don't need it
-                    if (GetTotalAllocatedBytesDelegate != null)
-                        return false;
-
-                    // check if monitoring is enabled
-                    if (!AppDomain.MonitoringIsEnabled)
-                        AppDomain.MonitoringIsEnabled = true;
-
-                    // verify the api works
-                    return AppDomain.MonitoringIsEnabled && AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize >= 0;
-                }
-                catch
-                {
-                    return false;
-                }
+                return false;
             }
         }
 #endif
-
     }
 }
